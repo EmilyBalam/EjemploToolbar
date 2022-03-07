d55b0da4465ac45166b1fbe1f3378869bea6990a
EmilyBalam <72897197+EmilyBalam@users.noreply.github.com>
2022-03-07 02:41:42 -0600

    Agregando mas funciones al Toolbar

--- app/src/main/java/com/example/ejemplotoolbar/MainActivity.kt
+++ app/src/main/java/com/example/ejemplotoolbar/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.ejemplotoolbar
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
@@ -15,5 +20,29 @@ class MainActivity : AppCompatActivity() {
         toolbar?.setTitle(R.string.app_name)
 
         setSupportActionBar(toolbar)
+        val bIr=findViewById<Button>(R.id.bIr)
+
+        bIr.setOnClickListener{
+            val intent = Intent(this,PantallaDos::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item?.itemId){
+            R.id.bFav ->{
+                Toast.makeText(this, "Elemento agregado como favorito",Toast.LENGTH_SHORT).show()
+                return true
+            }else->{
+            return super.onOptionsItemSelected(item)
+            }
+        }
+
+
     }
 }
